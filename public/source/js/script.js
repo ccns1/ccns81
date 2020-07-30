a89c9052787e8bc40f825fefb0c2ab202ec02d90
ccns1 <68824562+ccns1@users.noreply.github.com>
2020-07-30 02:14:54 +0200

    Update script.js

--- public/source/js/script.js
+++ public/source/js/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
      * The iceServers on this example are public and can be used for your project.
      */
     var peer = new Peer({
-        host: "localhost",
+        host: "ccns81.herokuapp.com",
         port: 9000,
         path: '/peerjs',
         debug: 3,
